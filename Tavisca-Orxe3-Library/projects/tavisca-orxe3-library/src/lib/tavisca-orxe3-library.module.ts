f9c57489bc476865d5d17c2b0f5af2682d1993e6
Saurabh Manchanda <smanchanda@tavisca.com>
2018-12-31 12:55:28 +0530

    Added - Input Component

--- Tavisca-Orxe3-Library/projects/tavisca-orxe3-library/src/lib/tavisca-orxe3-library.module.ts
+++ Tavisca-Orxe3-Library/projects/tavisca-orxe3-library/src/lib/tavisca-orxe3-library.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { TaviscaOrxe3LibraryComponent } from './tavisca-orxe3-library.component';
-import {MatButtonModule} from '@angular/material/button';
-import { TaviscaButtonComponent } from './tavisca-button/tavisca-button.component'
+import {MatButtonModule,MatInputModule} from '@angular/material';
+import { TaviscaButtonComponent } from './tavisca-button/tavisca-button.component';
+import { TaviscaInputComponent } from './tavisca-input/tavisca-input.component'
 
 @NgModule({
-  declarations: [TaviscaOrxe3LibraryComponent,TaviscaButtonComponent],
-  imports: [MatButtonModule],
-  exports: [TaviscaOrxe3LibraryComponent,TaviscaButtonComponent]
+  declarations: [TaviscaOrxe3LibraryComponent,TaviscaButtonComponent, TaviscaInputComponent],
+  imports: [MatButtonModule,MatInputModule],
+  exports: [TaviscaOrxe3LibraryComponent,TaviscaButtonComponent,TaviscaInputComponent]
 })
 export class TaviscaOrxe3LibraryModule { }
--- Tavisca-Orxe3-Library/projects/tavisca-orxe3-library/src/public_api.ts
+++ Tavisca-Orxe3-Library/projects/tavisca-orxe3-library/src/public_api.ts
@@ -6,3 +6,4 @@ export * from './lib/tavisca-orxe3-library.service';
 export * from './lib/tavisca-orxe3-library.component';
 export * from './lib/tavisca-orxe3-library.module';
 export * from './lib/tavisca-button/tavisca-button.component';
+export * from './lib/tavisca-input/tavisca-input.component';
