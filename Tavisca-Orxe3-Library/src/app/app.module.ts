5b58f1549c0846b86eb97084b023bc7e2e2b1bd3
Saurabh Manchanda <smanchanda@tavisca.com>
2019-03-09 14:13:10 +0530

    Removes Hotel-Item Import

--- Tavisca-Orxe3-Library/src/app/app.module.ts
+++ Tavisca-Orxe3-Library/src/app/app.module.ts
@@ -5,12 +5,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaviscaOrxe3LibraryModule } from 'tavisca-orxe3-library';
-import { HotelItemComponent } from './hotel-item/hotel-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HotelItemComponent
   ],
   imports: [
     BrowserModule,
